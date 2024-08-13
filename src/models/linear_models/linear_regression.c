39e0ae46a4efd9b1cf3dcb1ba32583ea7e90c2e6
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-08-13 22:09:18 +0700

    Add code

--- src/models/linear_models/linear_regression.c
+++ src/models/linear_models/linear_regression.c
@@ -9,3 +9,13 @@ void linear_regression_init(LinearRegression *model, size_t input_size)
     size_t layer_size[] = {input_size, 1};
     base_model_init(&(model->base), 2, layer_size);
 }
+
+void linear_regression_predict(LinearRegression *model, float *input, float *output)
+{
+    model->base.forward(&(model->base), input, output);
+}
+
+void linear_regression_free(LinearRegression *model)
+{
+    model->base.free(&(model->base));
+}
